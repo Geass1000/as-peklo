0d235f668232429067f2579475ce4cbe01b38e40
Andrey <geass1000@gmail.com>
2019-07-02 17:15:35 +0300

    feature(server): add `ArmoryType` enum

---
+++ server/modules/api/games/games.enums.ts
@@ -0,0 +1,11 @@
+
+export enum ArmoryType {
+  AirStrike = 'air_strike',
+  Medicaments = 'medicaments',
+  Gravibomb = 'gravibomb',
+  Shields = 'shields',
+  SpaceMines = 'space_mines',
+  RepairDrones = 'repair_drones',
+  AdaptiveShield = 'adaptive_shield',
+  Ecm = 'ecm',
+}
