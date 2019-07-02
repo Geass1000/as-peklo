7c90f3af99f9cc31defd1269ac5877d8440bdf96
Andrey <geass1000@gmail.com>
2019-07-02 19:33:23 +0300

    feature(client): register `core`, `material`, `form` modules and `signin` component

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,19 +1,27 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { MaterialModule } from './shared/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'web-api' }),
     BrowserTransferStateModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    MaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
