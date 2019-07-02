7fc5c50d59dbf610fe95693058d3d31d0d05917a
Andrey <geass1000@gmail.com>
2019-07-02 19:32:46 +0300

    feature(client): add `signin` route

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
