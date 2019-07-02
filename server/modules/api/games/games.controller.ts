aef5f8056ecf7a118b531290e61bfdabd2886547
Andrey <geass1000@gmail.com>
2019-07-02 17:16:10 +0300

    feature(server): add `Http` game service

--- server/modules/api/games/games.controller.ts
+++ server/modules/api/games/games.controller.ts
@@ -1,9 +1,12 @@
 import * as _ from 'lodash';
 
-import { APIController } from './../../../decorators';
+import { APIController, User } from './../../../decorators';
+
 import { GamesService } from './games.service';
+import * as Interfaces from './games.interfaces';
 import * as Nest from '@nestjs/common';
 import { GamesHelper } from './games.helper';
+import * as DTO from './games.dto';
 
 @APIController(1, 'games')
 export class GamesController {
@@ -12,6 +15,30 @@ export class GamesController {
   ) {}
 
   @Nest.Get()
-  async signIn() {
+  async stub() {
+  }
+
+  @Nest.Post()
+  async signIn (@Nest.Body() signInDto: DTO.SignIn) {
+    const sid = await this.gamesService.signIn({
+      uid: signInDto.uid,
+      auth_key: signInDto.auth_key,
+    });
+
+    return {
+      sid: sid
+    };
+  }
+
+  @Nest.Get()
+  async getGameInfo (@User() user: DTO.User): Promise<DTO.GameInfo> {
+    const gameInfo = await this.gamesService.getGameInfo(user);
+
+    const resourceItems = GamesHelper.getResourcesItems(gameInfo);
+    const armoryItems = GamesHelper.getArmoryItems(gameInfo);
+
+    return new DTO.GameInfo(
+      armoryItems, resourceItems,
+    );
   }
 }
