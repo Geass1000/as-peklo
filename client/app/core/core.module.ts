a6e86a6bc170a3556b5f255fa341291d0ef55c57
Andrey <geass1000@gmail.com>
2019-07-05 19:53:04 +0300

    feature(client): add `FA` icons

--- client/app/core/core.module.ts
+++ client/app/core/core.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { JwtModule } from '@auth0/angular-jwt';
 import { HttpClientModule } from '@angular/common/http';
 
+import { SharedModule } from '../shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faGooglePlusG, faFacebookF, faVk } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   imports: [
+    SharedModule,
     HttpClientModule,
     AuthModule,
   ],
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor() {
+    library.add(faTwitter);
+    library.add(faGooglePlusG);
+    library.add(faFacebookF);
+    library.add(faVk);
+  }
+}
