9f205ee2e73c116db0c6c7ecdd9072f875d69499
Andrey <geass1000@gmail.com>
2019-07-08 14:20:05 +0300

    feature(server): add facebook routes

--- server/modules/api/auth/auth.controller.ts
+++ server/modules/api/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { UserDocument } from '../user/shared/user.interfaces';
-import { GoogleGuard } from './guards/google.guard';
 import * as Nest from '@nestjs/common';
-
 import * as _ from 'lodash';
+
+import { UserDocument } from '../user/shared/user.interfaces';
+
+import { FacebookGuard } from './guards/facebook.guard';
+import { GoogleGuard } from './guards/google.guard';
 
 import * as Decorators from '../../../decorators';
 
@@ -16,6 +18,18 @@ export class AuthController {
   constructor (
     private authService: AuthService,
   ) {}
+
+  @Nest.Get(Constants.Routes.Facebook.Redirect)
+  public async facebookRedirectURI (): Promise<any> {
+    return this.authService.getFacebookRedirect();
+  }
+
+  @Nest.UseGuards(FacebookGuard)
+  @Nest.Get(Constants.Routes.Facebook.SignIn)
+  public async facebookSignIn (@Decorators.User() user: UserDocument): Promise<Interfaces.SignIn> {
+    const accessToken = this.authService.createToken(user);
+    return { accessToken: accessToken };
+  }
 
   @Nest.Get(Constants.Routes.Google.Redirect)
   public async googleRedirectURI (): Promise<any> {
