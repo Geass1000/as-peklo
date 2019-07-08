33569d89ff5c0d7af4fa2b9e1a4badcca95e0593
Andrey <geass1000@gmail.com>
2019-07-08 17:40:46 +0300

    feature(client): add `Auth` guard on profile route

--- client/app/app.router.ts
+++ client/app/app.router.ts
@@ -1,8 +1,11 @@
-import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './core/auth/auth.guard';
+
 import * as AuthConstants from './auth/shared/auth.constants';
+
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
@@ -16,6 +19,7 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then(mod => mod.ProfileModule),
+    canActivate: [ AuthGuard ],
   },
 ];
 
