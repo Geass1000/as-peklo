f5e4629ed4c6567d3ab82dfc47685de9ffff4b2b
Andrey <geass1000@gmail.com>
2019-07-08 12:35:18 +0300

    feature(client): add `sign out` component

---
+++ client/app/auth/signout/signout.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './../../core/auth/auth.service';
+
+@Component({
+  selector: 'app-signout',
+  templateUrl: './signout.component.html',
+  styleUrls: ['./signout.component.scss']
+})
+export class SignOutComponent implements OnInit {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) { }
+
+  public ngOnInit() {
+    this.authService.signOut()
+      .subscribe(() => {
+        this.router.navigate([ `/` ], { replaceUrl: true });
+      });
+  }
+}
