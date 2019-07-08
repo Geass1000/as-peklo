83aed11d69d49a39028e506e6ca7695e4272c0ff
Andrey <geass1000@gmail.com>
2019-07-08 12:11:50 +0300

    feature(server): use new `callback`redirect url

--- server/modules/api/auth/configs/google.config.ts
+++ server/modules/api/auth/configs/google.config.ts
@@ -6,7 +6,7 @@ export const GoogleConfig: Interfaces.Config.Google = {
   clientId: '764017121074-7rnlv0i9t5ju1p7j56k028rkcum4p77t.apps.googleusercontent.com',
   clientSecret: 'ArMYhmM-xBbUfweOp_FVFZzJ',
   loginDialogURL: 'https://accounts.google.com/o/oauth2/auth',
-  oauthRedirectURL: `${environment.server.protocol}://${environment.server.domain}:4200/recipes`,
+  oauthRedirectURL: `${environment.server.protocol}://${environment.server.domain}:4200/auth/callback`,
   responseType: 'code',
   scopes: [
     'profile',
