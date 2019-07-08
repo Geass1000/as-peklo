2f1b3a59e0d40ab6a77547cd2bc170db96b5ca7d
Andrey <geass1000@gmail.com>
2019-07-08 12:10:15 +0300

    feature(client): implement google, token logic

--- client/app/core/auth/auth.service.ts
+++ client/app/core/auth/auth.service.ts
@@ -1,15 +1,96 @@
+import { Observable } from 'rxjs/Observable';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { SignIn } from './auth.interfaces';
+import { map } from 'rxjs/operators';
+
+import * as Constants from './auth.constants';
+import * as Interfaces from './auth.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  private apiV1 = `api/v1/`;
+  private apiAuth = `${this.apiV1}/auth`;
 
-  public signIn (signInData: SignIn) {
-    return this.http.post(`api/v1/games/signin`, signInData);
+  constructor(
+    private http: HttpClient,
+    private jwtHelperService: JwtHelperService,
+  ) {}
+
+  /**
+   * Provider logic
+   */
+
+  public getGoogleRedirectURL () {
+    return this.http.get(`${this.apiAuth}/google/redirect`);
+  }
+
+  public googleSignIn (code: string) {
+    const req = this.http.get<Interfaces.SignIn>(`${this.apiAuth}/google/signin?code=${code}`);
+    return this.signInHandler(req);
+  }
+
+  /**
+   * Auth logic
+   */
+
+  public signOut () {
+    localStorage.removeItem(Constants.LocalStorage.AccessToken);
+  }
+
+  public refreshToken () {
+    const req = this.http.get<Interfaces.SignIn>(`api/v1/auth/refresh`);
+    return this.signInHandler(req);
+  }
+
+  private signInHandler (req: Observable<Interfaces.SignIn>): Observable<Interfaces.SignIn> {
+    return req.pipe(map((data) => {
+      localStorage.setItem(Constants.LocalStorage.AccessToken, data.accessToken);
+      return data;
+    }));
+  }
+
+  /**
+   * Token logic
+   */
+
+  public setToken (token: Interfaces.AccessToken.Type): void {
+    localStorage.setItem(Constants.LocalStorage.AccessToken, token);
+  }
+
+  public getToken (): Interfaces.AccessToken.Type {
+    const accessToken: Interfaces.AccessToken.Type =
+      localStorage.getItem(Constants.LocalStorage.AccessToken);
+    return accessToken;
+  }
+
+  public getTokenData (): Interfaces.AccessToken.Data {
+    const accessToken: Interfaces.AccessToken.Type =
+      localStorage.getItem(Constants.LocalStorage.AccessToken);
+
+    try {
+      return this.jwtHelperService.decodeToken(accessToken);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  public isLoggedIn (): boolean {
+    const tokenData = this.getTokenData();
+    return tokenData !== null;
+  }
+
+  public isExpired (): boolean {
+    const accessToken: Interfaces.AccessToken.Type =
+      localStorage.getItem(Constants.LocalStorage.AccessToken);
+
+    try {
+      return !this.jwtHelperService.isTokenExpired(accessToken);
+    } catch (error) {
+      return false;
+    }
   }
 }
