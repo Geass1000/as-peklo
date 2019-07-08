cbaf69bd4851f4c13b9fc8d0e10a4aa10feec5d9
Andrey <geass1000@gmail.com>
2019-07-08 12:35:59 +0300

    feature(client): add `sign out` and `sign in` routes

--- client/app/auth/auth.router.ts
+++ client/app/auth/auth.router.ts
@@ -1,14 +1,23 @@
+import { SignInComponent } from './signin/signin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
+import { SignOutComponent } from './signout/signout.component';
 import { CallbackComponent } from './callback/callback.component';
 
 import * as Constants from './shared/auth.constants';
 
 const routes: Routes = [
-  { path: Constants.Route.SignIn, component: AuthComponent, },
-  { path: `callback`, component: CallbackComponent, },
+  { path: Constants.Route.SignIn,
+    component: AuthComponent,
+    children: [
+      { path: Constants.Route.SignIn, component: SignInComponent, },
+      { path: `signin`, redirectTo: `/${Constants.Route.Base}`, },
+      { path: `callback`, component: CallbackComponent, },
+      { path: Constants.Route.SignOut, component: SignOutComponent, },
+    ],
+  },
 ];
 
 @NgModule({
@@ -20,4 +29,6 @@ export class AuthRouter { }
 export const components = [
   AuthComponent,
   CallbackComponent,
+  SignInComponent,
+  SignOutComponent,
 ];
