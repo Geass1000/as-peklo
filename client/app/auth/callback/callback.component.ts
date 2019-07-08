1ff3a848029ad9d2bc6a3cca61314975a8366223
Andrey <geass1000@gmail.com>
2019-07-08 11:04:17 +0300

    feature(client): add OAuth `callback` page

---
+++ client/app/auth/callback/callback.component.ts
@@ -0,0 +1,50 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './../../core/auth/auth.service';
+
+@Component({
+  selector: 'app-callback',
+  templateUrl: './callback.component.html',
+  styleUrls: ['./callback.component.scss']
+})
+export class CallbackComponent implements OnInit {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    @Inject(PLATFORM_ID) private readonly platformId: any,
+  ) { }
+
+  public ngOnInit() {
+    const inBrowser = isPlatformBrowser(this.platformId);
+
+    if (!inBrowser) {
+      return;
+    }
+
+    this.route.queryParamMap.subscribe((params) => {
+      let signInResult: Observable<any> = null;
+
+      if (params.has('code') && params.has('state')) {
+        // Facebook Login
+      } else if (params.has('oauth_token') && params.has('oauth_verifier')) {
+        // Twitter Login
+      } else if (params.has('code')) {
+        const code = params.get('code');
+        // Google Login
+        signInResult = this.authService.googleSignIn(code);
+      }
+
+      if (signInResult === null) {
+        return;
+      }
+
+      signInResult.subscribe(() => {
+        this.router.navigate([ `/` ], { replaceUrl: true });
+      });
+    });
+  }
+}
