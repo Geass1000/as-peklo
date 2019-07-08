83b225128cb3bb5dccbdd0e6d53f367812e54d59
Andrey <geass1000@gmail.com>
2019-07-08 16:45:25 +0300

    feature(server): add `vk` guard

---
+++ server/modules/api/auth/guards/vkontakte.guard.ts
@@ -0,0 +1,24 @@
+import * as Nest from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import * as Constants from '../shared/auth.constants';
+import * as Interfaces from '../shared/auth.interfaces';
+
+@Nest.Injectable()
+export class VkontakteGuard extends AuthGuard('vkontakte') {
+  constructor (
+    @Nest.Inject(Constants.DI.Config.Vkontakte)
+      private readonly config: Interfaces.Config.Vkontakte,
+  ) {
+    super({ scope: config.scopes });
+  }
+
+  async canActivate (context: Nest.ExecutionContext): Promise<any> {
+    try {
+      const res = await super.canActivate(context);
+      return res;
+    } catch (error) {
+      return false;
+    }
+  }
+}
