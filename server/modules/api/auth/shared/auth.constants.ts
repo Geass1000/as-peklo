9749f354806016bd2ceebc25d6404bbef831e733
Andrey <geass1000@gmail.com>
2019-07-08 14:14:33 +0300

    feature(server): add `facebook` constants

--- server/modules/api/auth/shared/auth.constants.ts
+++ server/modules/api/auth/shared/auth.constants.ts
@@ -2,12 +2,19 @@
 export namespace DI {
   export namespace Config {
     export const Google = Symbol(`DI-Config-Google`);
+    export const Facebook = Symbol(`DI-Config-Facebook`);
   }
 }
 
 export namespace Routes {
   export namespace Google {
     const BaseURI = `google`;
+    export const Redirect: string = `${BaseURI}/redirect`;
+    export const SignIn: string = `${BaseURI}/signin`;
+  }
+
+  export namespace Facebook {
+    const BaseURI = `facebook`;
     export const Redirect: string = `${BaseURI}/redirect`;
     export const SignIn: string = `${BaseURI}/signin`;
   }
