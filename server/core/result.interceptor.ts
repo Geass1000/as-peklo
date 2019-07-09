447c68c5a58711714733ae0dc15232869065171f
Andrey <geass1000@gmail.com>
2019-07-09 11:35:35 +0300

    feature(server): use `API` interfaces for response type

--- server/core/result.interceptor.ts
+++ server/core/result.interceptor.ts
@@ -2,6 +2,8 @@ import * as Nest from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { API } from '../../shared/interfaces';
+
 /**
  * Converts response data to `{ result: data }` JSON format.
  *
@@ -9,7 +11,7 @@ import { map } from 'rxjs/operators';
  */
 @Nest.Injectable()
 export class ResultInterceptor implements Nest.NestInterceptor {
-  public intercept(context: Nest.ExecutionContext, next: Nest.CallHandler): Observable<any> {
+  public intercept (context: Nest.ExecutionContext, next: Nest.CallHandler): API.RxResult<any> {
     return next
       .handle()
       .pipe(
