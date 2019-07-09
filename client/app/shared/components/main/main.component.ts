d091d018e948c52954bcba0a8a9546187d3df250
Andrey <geass1000@gmail.com>
2019-07-09 12:46:13 +0300

    feature(client): add `Main` shared component

---
+++ client/app/shared/components/main/main.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent {
+}
--- client/app/shared/shared.module.ts
+++ client/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MainComponent } from './components/main/main.component';
 
 
 export const modules = [
@@ -11,15 +12,22 @@ export const modules = [
   FontAwesomeModule,
 ];
 
+export const components = [
+  MainComponent,
+];
+
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     ...modules,
   ],
+  declarations: [
+    ...components,
+  ],
   exports: [
     CommonModule,
     ...modules,
-  ]
+    ...components,
+  ],
 })
 export class SharedModule {}
