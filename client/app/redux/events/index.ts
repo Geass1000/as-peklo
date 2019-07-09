aa9e4aace7d7b8bdbb2cebd5865127584b63d2d2
Andrey <geass1000@gmail.com>
2019-07-09 11:45:09 +0300

    feature(client): add `SetUserId` and `SetUserSocials` profile events

---
+++ client/app/redux/events/index.ts
@@ -0,0 +1,7 @@
+import * as App from './app.events';
+import * as Profile from './profile.events';
+
+export {
+  App,
+  Profile,
+};
---
+++ client/app/redux/events/profile.events.ts
@@ -0,0 +1,4 @@
+const ActionName = `ProfileAction`;
+
+export const SetUserId = `${ActionName}-SetUserId`;
+export const SetUserSocials = `${ActionName}-SetUserSocials`;
