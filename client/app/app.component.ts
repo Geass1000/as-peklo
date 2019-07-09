65a4aa277df7e731b5229c253cbb384124919765
Andrey <geass1000@gmail.com>
2019-07-09 12:09:03 +0300

    feature(client): set `UserID` into Redux after `Bootstrap` operation

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './core/auth/auth.service';
+import { ProfileAction } from './redux/actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'universal';
+
+  constructor(
+    private readonly authService: AuthService,
+    private profileAction: ProfileAction,
+  ) { }
+
+  public ngOnInit (): void {
+    const tokenData = this.authService.getTokenData();
+
+    if (tokenData === null) {
+      return;
+    }
+
+    this.profileAction.setUserId(tokenData.userId);
+  }
 }
