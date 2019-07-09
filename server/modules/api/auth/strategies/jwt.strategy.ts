aae66a989d67d0d6f4ad074ccd5d3beb5c693bee
Andrey <geass1000@gmail.com>
2019-07-09 16:34:54 +0300

    feature(server): use `Token` service to validate a token

--- server/modules/api/auth/strategies/jwt.strategy.ts
+++ server/modules/api/auth/strategies/jwt.strategy.ts
@@ -1,17 +1,16 @@
 import * as Nest from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 import { environment } from '../../../../environments/environment';
 
-import { AuthService } from '../auth.service';
+import { TokenService } from '../token.service';
 
 import * as Interfaces from '../shared/auth.interfaces';
 
 @Nest.Injectable()
 export class JWTStrategy extends PassportStrategy(Strategy) {
-  constructor (private readonly authService: AuthService) {
+  constructor (private readonly tokenService: TokenService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: environment.auth.jwt.secret,
@@ -19,7 +18,7 @@ export class JWTStrategy extends PassportStrategy(Strategy) {
   }
 
   public async validate (data: Interfaces.AccessToken.Data, done: Function) {
-    const isValid = await this.authService.validateToken(data);
+    const isValid = await this.tokenService.validateAccessToken(data);
 
     if (!isValid) {
       return done(new Nest.UnauthorizedException(), null);
