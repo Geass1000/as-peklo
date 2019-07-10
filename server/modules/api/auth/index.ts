8d81fd3cdd9a754872996e4682e65836c7f31b39
Andrey <geass1000@gmail.com>
2019-07-10 12:52:19 +0300

    feature(server): change `reexport` variables (add Decorators, Guards, Shared and remove all previous)

--- server/modules/api/auth/index.ts
+++ server/modules/api/auth/index.ts
@@ -1,2 +1,9 @@
-export * from './shared';
-export * from './auth.service';
+import * as Decorators from './decorators';
+import * as Guards from './guards';
+import * as Shared from './shared';
+
+export {
+  Decorators,
+  Guards,
+  Shared,
+};
