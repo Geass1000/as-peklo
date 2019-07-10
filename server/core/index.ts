c4be061f8f0075e2d6514ff1c9ed61a9aeaf26bc
Andrey <geass1000@gmail.com>
2019-07-10 12:58:29 +0300

    feature(server): add `reexport` logic for `core` interceptors

--- server/core/index.ts
+++ server/core/index.ts
@@ -1,7 +1,9 @@
+import * as Interceptorrs from './interceptors';
 import * as Decorators from './decorators';
 import * as Exceptions from './exceptions';
 
 export {
+  Interceptorrs,
   Decorators,
   Exceptions,
 };
