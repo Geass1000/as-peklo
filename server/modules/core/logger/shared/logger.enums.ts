e1259351c8796d2a6e83a5f1fac931607fbe4fac
Andrey <geass1000@gmail.com>
2019-07-10 18:18:06 +0300

    feature(server): add `bright` colors

--- server/modules/core/logger/shared/logger.enums.ts
+++ server/modules/core/logger/shared/logger.enums.ts
@@ -27,6 +27,14 @@ export enum TextColor {
   Magenta = '\x1b[35m',
   Cyan = '\x1b[36m',
   White = '\x1b[37m',
+  BrightBlack = '\x1b[90m',
+  BrightRed = '\x1b[91m',
+  BrightGreen = '\x1b[92m',
+  BrightYellow = '\x1b[93m',
+  BrightBlue = '\x1b[94m',
+  BrightMagenta = '\x1b[95m',
+  BrightCyan = '\x1b[96m',
+  BrightWhite = '\x1b[97m',
 }
 
 export enum BgColor {
@@ -38,5 +46,13 @@ export enum BgColor {
   Magenta = '\x1b[45m',
   Cyan = '\x1b[46m',
   White = '\x1b[47m',
+  BrightBlack = '\x1b[100m',
+  BrightRed = '\x1b[101m',
+  BrightGreen = '\x1b[102m',
+  BrightYellow = '\x1b[103m',
+  BrightBlue = '\x1b[104m',
+  BrightMagenta = '\x1b[105m',
+  BrightCyan = '\x1b[106m',
+  BrightWhite = '\x1b[107m',
 }
 
