020d6dd9debee3cd00954f7b9d04603f21e3a67e
Andrey <geass1000@gmail.com>
2019-07-10 12:36:27 +0300

    feature(shared): add `roles` field to data of access token

--- shared/interfaces/auth.interfaces.ts
+++ shared/interfaces/auth.interfaces.ts
@@ -1,9 +1,11 @@
+import * as Enums from '../enums';
 
 export namespace AccessToken {
   export type Type = string;
 
   export interface Data {
     userId: string;
+    roles: Enums.Auth.Roles[];
   }
 }
 
