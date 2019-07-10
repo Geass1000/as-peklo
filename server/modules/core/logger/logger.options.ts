bb00139d4a60e1871347cf5ba28761c183d0296f
Andrey <geass1000@gmail.com>
2019-07-10 19:13:39 +0300

    feature(server): add new styles to default options

--- server/modules/core/logger/logger.options.ts
+++ server/modules/core/logger/logger.options.ts
@@ -8,15 +8,36 @@ export const DefaultOptions: Interfaces.Options = {
   },
   styles: {
     disabled: false,
-    [`${Enums.LogLevel.Info}`]: {
+    common: {
       colors: {
-        logLevel: Enums.TextColor.Green,
-        message: Enums.TextColor.White,
+        timestamp: Enums.TextColor.White,
+        className: Enums.TextColor.Yellow,
+        methodName: Enums.TextColor.Green,
       },
     },
     [`${Enums.LogLevel.Error}`]: {
       colors: {
-        logLevel: Enums.TextColor.Red,
+        logLevel: Enums.TextColor.BrightRed,
+      },
+    },
+    [`${Enums.LogLevel.Warn}`]: {
+      colors: {
+        logLevel: Enums.TextColor.BrightYellow,
+      },
+    },
+    [`${Enums.LogLevel.Info}`]: {
+      colors: {
+        logLevel: Enums.TextColor.Green,
+      },
+    },
+    [`${Enums.LogLevel.Debug}`]: {
+      colors: {
+        logLevel: Enums.TextColor.BrightMagenta,
+      },
+    },
+    [`${Enums.LogLevel.Log}`]: {
+      colors: {
+        logLevel: Enums.TextColor.BrightCyan,
       },
     },
   }
